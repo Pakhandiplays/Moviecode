8dd744f18b25b5e3f32e2db7845ab952090ad839
Pakhandiplays <s.k7557442799@gmail.com>
2024-06-28 20:56:56 +0530

    update

--- js/nevbar.js
+++ js/nevbar.js
@@ -3,7 +3,7 @@ const navbarToggle = navbar.querySelector("#navbar-toggle");
 const navbarMenu = document.querySelector("#navbar-menu");
 document.getElementById('nothing_1').value = "Sumit kumar";
 document.getElementById('nothing_2').value = "codebysumit";
-document.getElementById('usefull').href = "/Admin/qwertyuiopasdfghjklzxcvbnm.html"
+document.getElementById('usefull').href = "Admin/qwertyuiopasdfghjklzxcvbnm.html"
 const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
 let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
 document.getElementById('nothing_1').style.display = "none";
