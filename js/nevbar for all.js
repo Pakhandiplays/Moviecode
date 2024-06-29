ff09dc21a46d66024d87d2033ac78ee820ff50e2
Pakhandiplays <s.k7557442799@gmail.com>
2024-06-29 08:01:16 +0530

    update

--- js/nevbar for all.js
+++ js/nevbar for all.js
@@ -1,13 +1,8 @@
 // nevbar
 const navbarToggle = navbar.querySelector("#navbar-toggle");
 const navbarMenu = document.querySelector("#navbar-menu");
-document.getElementById('nothing_1').value = "Sumit kumar";
-document.getElementById('nothing_2').value = "codebysumit";
-document.getElementById('usefull').href = "Admin/qwertyuiopasdfghjklzxcvbnm.html"
 const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
 let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
-document.getElementById('nothing_2').style.display = "none";
-document.getElementById('nothing_1').style.display = "none";
 const toggleNavbarVisibility = () => {
   isNavbarExpanded = !isNavbarExpanded;
   navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
