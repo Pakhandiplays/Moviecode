29f987bb6541f06d70e4d3191d975fbdced6bfe0
Pakhandiplays <s.k7557442799@gmail.com>
2024-06-30 12:54:51 +0530

    Update download.js

--- js/download.js
+++ js/download.js
@@ -31,8 +31,8 @@ function puality() {
         alert("Your Quality is selected")
       document.getElementById('chapter_1').href = "https://sparkling-frost-6f58.viliwe4328311.workers.dev/1396603470/609ea8daa29bc6e3bca7445a17e346143aae2350e04332f10512b49059352da86202880683726a0c03a8cac71602e37d6fcd69aba057e1dcf65d550e21b31bcba9b585eeae0ddab4ea7918fbe0c2fb8e08a2bced5d0282f98d8ceac09d877abf682f6ecb919892a8dc2cdca9d67fd76b9b2e33124243aea6384b8a5f7256314de1c6d492fa8adfe0470c22a0bd392279::ab7cadf9590c4f510a2f8b24d20b9c8c/2_Harry.Potter.and.the.Sorcerers.Stone.2001.480p.Hindi.Eng.Vegamovies.to.mkv"
       document.getElementById('chapter_2').href = "https://ancient-king-5b05.dolek225091209.workers.dev/1396603470/728b5d1a8446ba01f818fbc644ca59d35cda5955d75a44767c28c3a51d6072b844780e2d7de28ba4ef06e52eeeb3465f2c28462dbf5fbe0d10de434d237069e26d1805b542d1886459e0b6a1a3e0e62428794ea7c791d17ebf7d4f2e1536770a4c65013ee96a1e3ed75d29f8ec778725512ce3dc741b446da3f1c74842f46c65d851bc703ee662252dfb0f3f4f6e632e::96e0385b96d52262fab16d5e37457104/Harry.Potter.and.the.Chamber.of.Secrets.2002.480p.Hindi.Eng.Vegamovies.NL.mkv"
-      document.getElementById('chapter_3').href = ""
-      document.getElementById('chapter_4').href = ""
+      document.getElementById('chapter_3').href = "https://cold-heart-2dae.remewel8854930.workers.dev/1396603470/6d539e188cbb74f48e9461b66ec71758e5c7b04aeff68055cf1ab5626f22254bbdece93b82bd0d64d5cf094d93671e241c6d41ed23b21dc126c46872bcf5a76aae0cd309af31bcd312802f41274bfc91f675ec302efbc26fd7e9c968a12bce6b5613cbac7235c712ed76ad9d96d9e409c1e53ba4815bf997c2229b7d1ca4556b1770371b7ee71ed52e008c1957281a53::86eaf64a1e9da3d4859b1db3891412c4/Harry.Potter.and.the.Prisoner.of.Azkaban.2004.480p.BluRay.Hindi.English.Vegamovies.NL.mkv"
+      document.getElementById('chapter_4').href = "https://little-disk-5ab5.hebiw667488512.workers.dev/1396603470/b17e6208f006ac435a4bbfdbfdd442bc27c2a88a02530583181d3bbbf1973658f70dab089b6dc2368ca10af556da5eb468e28a4417a87ca20448e71c2295d7de38748d4c32de16eee12c3c2b534f2c60621ec3afe244420da52bb7bc2ed73fcab70cb39119de83b5c700da680c380fb03a683ba0f22d1c12e162d7e6ec2e5b37::ab0661d2fd972115d46c2d43e0d7bd5c/Harry.Potter.and.the.Goblet.of.Fire.2005.480p.Hindi.Eng.Vegamovies.NL.mkv"
       document.getElementById('chapter_5').href = ""
       document.getElementById('chapter_6').href = ""
       document.getElementById('chapter_7').href = ""
@@ -43,8 +43,8 @@ function puality() {
         alert("Your Quality is selected")
         document.getElementById('chapter_1').href = "https://fragrant-darkness-e958.nihac367878182.workers.dev/1396603470/bb5f4a0f33f624f0010560b6e16c620bafd5cbb6c1ea99efe8befee05f1395c10df5f33cfa53857bc4bae2b4030f803cdba7aa7e8768ef4a5da7cd921d97fb796b1decde53cb9a84a6bbf57534213fa4d7ab9da31ea28b8708a6e7a25e824cfe2997147affc0b5252770fbb3fa0111f73a5497df0535cc7f49b6c13c0f174ece::455adf24726efabd11491a3093d9b639/Harry.Potter.1.2001.720p.HEVC.BluRay.Dual.x265.ESub.-.Vegamovies.NL.mkv"
         document.getElementById('chapter_2').href = "https://icy-sea-f936.vemimej3394175.workers.dev/1396603470/f50a8d37343d88f388eec7b7cf392be2138716fb4d126913a3d5b1082b5db0a4329c369b0ab02ea086f2cc7f605ab558c432360e719ed71253f03140456186f77098971a0eb82624ddeb21b3625625bf002196656fd21dea521bf272498b143d5c722b0ede36fd711ad270a9420a6ef32f1c83db5e4390f6c05785ad3d1699eb9521aff5877ff55889e01c28d679b6b0545703b0ecb50baf15537128141e4a12::8f4d3e8fec99ab8ed2f3706c7cf81626/Harry.Potter.and.the.Chamber.of.Secrets.2002.720p.HEVC.BluRay.Dual.Audio.x265.ESub.-.Vegamovies.NL.mkv"
-        document.getElementById('chapter_3').href = ""
-        document.getElementById('chapter_4').href = ""
+        document.getElementById('chapter_3').href = "https://wild-mountain-832d.bisal211669633.workers.dev/1396603470/320f61264032cec1dc3a5583bd39e2bf692cce5c85dbbe80b5e4abdc46560e324203ac9e9dbc7d5ff56b8b0a046acde960ef31ed75d87b7a7ab315e9927181e0a78bdb927bee59e38bb678b912f271b6cea77160124b7235d1251d93d3e3d5b1bd0bc22f1f65dde955ca71fda4103eb57634dcaf5bfc8280b30c2f1f651a1572423ac3ed879764b9c2d31fc15d45ace0b7ae52645bf0442f1c1939dd8a1765fc::e156a0dc24d212e765674fef2a3c2bd0/Harry.Potter.and.the.Prisoner.of.Azkaban.2004.720p.HEVC.BluRay.Dual.Audio.x265.ESub.-.Vegamovies.NL.mkv"
+        document.getElementById('chapter_4').href = "https://little-disk-5ab5.hebiw667488512.workers.dev/1396603470/b17e6208f006ac435a4bbfdbfdd442bc27c2a88a02530583181d3bbbf1973658f70dab089b6dc2368ca10af556da5eb468e28a4417a87ca20448e71c2295d7de38748d4c32de16eee12c3c2b534f2c60621ec3afe244420da52bb7bc2ed73fcab70cb39119de83b5c700da680c380fb03a683ba0f22d1c12e162d7e6ec2e5b37::ab0661d2fd972115d46c2d43e0d7bd5c/Harry.Potter.and.the.Goblet.of.Fire.2005.480p.Hindi.Eng.Vegamovies.NL.mkv"
         document.getElementById('chapter_5').href = ""
         document.getElementById('chapter_6').href = ""
         document.getElementById('chapter_7').href = ""
@@ -55,8 +55,8 @@ function puality() {
         alert("Your Quality is selected")
         document.getElementById('chapter_1').href = "https://calm-cell-9905.dawot383587871.workers.dev/1396603470/3e98ef72a864f67769ac40f281eeb337f461761f64008fadef584ee931ca357e1dcb04af1bada9f86ce45ff3c904cf9dac9d00f04b6a85c6dfaef101daa708ee77f89c11185f9a77a5317458a61873d999297fc7abdd02f5c048e88eace297412719a659181b11c99366adbe1f6e2b8e39cdf3b3c9975affcbc0f22c0bcadc137a5b652d6e1ba9e488d08e9b5b8d6804::47ba32e368e3d4cf756826ac858d9a1e/Harry.Potter.and.the.Sorcerers.Stone.2001.720p.Hindi.Eng.Vegamovies.to.mkv"
         document.getElementById('chapter_2').href = "https://cold-heart-2dae.remewel8854930.workers.dev/1396603470/4725640e40eb68d944f61c5f0bda4b07530a52d1ee3ab23826af1e512030cfb7e3338c9607d833398d7381afc66c0819bbf889e40a9cdae2f2382877f2c2c395b86dcf4a11d8ff1280e1e30410884eae3d3520159be23c18e417bc673b07384168d4590257c7cade3e2118b299fc8cb14b4a88a78e4a980eecd07a095e205278f076349c40f4a3ea4c6efeccd8812d63::52b110f1d9d472a27bc2ec313f9b2685/Harry.Potter.and.the.Chamber.of.Secrets.2002.720p.Hindi.Eng.Vegamovies.NL.mkv"
-        document.getElementById('chapter_3').href = ""
-        document.getElementById('chapter_4').href = ""
+        document.getElementById('chapter_3').href = "https://cool-night-a8c1.wetoh780378829.workers.dev/1396603470/c3f3ae2d2e9c91ad3c5411beae0b2dee170c33b6596167ed90ed07eedd9f5dc7a5528943b8745153bc36710010265975f0b9d092b9eaf2aa13e1a61a19f1bc6e3d144c4ad0e2b7121237bcee9d58beee4ff1d7a24d7112070a1eedb0f9eee0dcfc9fe3d8b85f64ac5282d82d367d456898fbc1a6dfcca0a9d1e3c119b15215d3ef488a1623fe3bd881aa954aaa748cb1::cacf9a283d0f8232d7555c454acf42a7/Harry.Potter.and.the.Prisoner.of.Azkaban.2004.720p.BluRay.Hindi.English.Vegamovies.NL.mkv"
+        document.getElementById('chapter_4').href = "https://hidden-boat-e87c.hivegic619569.workers.dev/1396603470/0efa1e3f879cbbcaffd8b5c693916abb701eebc8ed11c8b7dac84a48fb2f51ddd8e2f6de73c77eb1b4df232adc91c4a779d4c3da24304b16b573fb4ca5ae0bfb2e76da1954a6d5427cf8ff412ae76ecd6c253136b7c3f5de24b0180ee4bdd4b865eae6887066544f794ad53cf86f978d457b859aa3c9fddb95349268ea178da1::674e6ab1ffb285816ae2d6c6a38edd0d/Harry.Potter.and.the.Goblet.of.Fire.2005.720p.Hindi.Eng.Vegamovies.NL.mkv"
         document.getElementById('chapter_5').href = ""
         document.getElementById('chapter_6').href = ""
         document.getElementById('chapter_7').href = ""
@@ -67,8 +67,8 @@ function puality() {
         alert("Your Quality is selected")
         document.getElementById('chapter_1').href = "https://pub-dfa1641e7f48495a9ef0af3d70b13584.r2.dev/1.%20Harry%20Potter%20and%20the%20Sorcerers%20Stone%202001%20Extended%2010bit%20BluRay%20x265%20HEVC%20[Org%20DD%202.0%20Hindi%20+%20DD%205.1%20Eng]%20ESubs%201080p%20-%20mkvCinemas.mkv"
         document.getElementById('chapter_2').href = "https://pub-58c47f148cd84e349d61ac705d81efd1.r2.dev/2_2.%20Harry%20Potter%20and%20the%20Chamber%20of%20Secrets%202002%20Extended%2010bit%20BluRay%20x265%20HEVC%20[Org%20DD%202.0%20Hindi%20+%20DD%205.1%20Eng]%20ESubs%201080p%20-Vegamovies.to.mkv"
-        document.getElementById('chapter_3').href = ""
-        document.getElementById('chapter_4').href = ""
+        document.getElementById('chapter_3').href = "https://pub-31efc4f5475a46d7a78a640997b2dafd.r2.dev/3.%20Harry%20Potter%20and%20the%20Prisoner%20of%20Azkaban%202004%20BluRay%20x265%20[Org%20DD%20Hindi%20+%20DD%205.1%20Eng]%20ESubs%201080p%20-%20Vegamovies.NL.mkv"
+        document.getElementById('chapter_4').href = "https://pixeldra.in/api/file/oEme4vQ2?download"
         document.getElementById('chapter_5').href = ""
         document.getElementById('chapter_6').href = ""
         document.getElementById('chapter_7').href = ""
